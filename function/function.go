bc1db3011a65c551ea0303f1c8c7b6eec8aeb9cd
OneJ <3942241@qq.com>
2018-01-16 00:23:50 +0800

    +

--- function/function.go
+++ function/function.go
@@ -1,5 +1,6 @@
 package main
 
+//
 import (
 	"fmt"
 	"math"
@@ -13,8 +14,21 @@ func (p *Point) Distance(q Point) float64 {
 	return math.Hypot(q.X-p.X, q.Y-p.Y)
 }
 
+type Path []Point
+
+func (path Path) Distance() float64 {
+	var sum float64
+	for i := 0; i < path.LenPoints()-1; i++ {
+		sum = [i]path + [i+1]path
+	}
+	return 0
+}
+
+func (path Path) LenPoints() int {
+	return len(path)
+}
+
 func main() {
-	p := Point{1, 2}
-	q := Point{4, 6}
-	fmt.Println(p.Distance(q))
+	path := Path{{1, 2}, {3, 4}, {5, 6}}
+	fmt.Println(path.Distance())
 }
